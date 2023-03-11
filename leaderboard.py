7e4f0f23b0807e5fcb3f1b1f4f67dce2e67f6a4a
joe-mcdonald <joemcd0224@gmail.com>
2023-03-11 15:53:41 -0800

    cleaned up code

--- leaderboard.py
+++ leaderboard.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 import requests
+import json
 
-response = requests.get('http://127.0.0.1:8000/')
+response_API = requests.get('http://127.0.0.1:8000/')
 
-if response.status_code == 200:
-    data = response.json()
-    print(type(data))
-    # data.keys()
-    print(data.values())
+if response_API.status_code == 200:
+    data = response_API.text
+    parse_json = json.loads(data)
+    
+    print(parse_json['data'][0]['position'])
 else:
     print("Error: Could not retrieve data from API.")
